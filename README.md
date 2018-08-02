172e426feeb6c149fe047cfe6e804fe0493f70c7
David A. Viramontes <dviramontes@gmail.com>
2018-08-01 22:49:11 -0600

    adds shell scripts

---
+++ README.md
@@ -0,0 +1,10 @@
+## closet of souls
+
+### running
+```
+./copy-ssh-keys.sh
+./listen-all.sh
+
+from master 
+- start stream
+```
---
+++ copy-ssh-keys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# run ssh-keygen if you haven't already
+# and then...
+
+ssh-copy-id pi@192.168.50.34
+ssh-copy-id pi@192.168.50.47
+ssh-copy-id pi@192.168.50.92
+ssh-copy-id pi@192.168.50.13
+ssh-copy-id pi@192.168.50.199
+ssh-copy-id pi@192.168.50.83
