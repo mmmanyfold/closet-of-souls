fe4374f2402c37d6a9c341e3e60254d366e47c4f
David A. Viramontes <dviramontes@gmail.com>
2018-08-04 00:31:31 -0700

    adds update-tiles.sh

---
+++ update-tiles.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+scp .piwall pi@192.168.29.234:/home/pi
+scp .piwall pi@192.168.29.245:/home/pi
+scp .piwall pi@192.168.29.241:/home/pi
+scp .piwall pi@192.168.29.148:/home/pi
+scp .piwall pi@192.168.29.249:/home/pi
+scp .piwall pi@192.168.29.213:/home/pi
