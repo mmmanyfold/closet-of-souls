d2895167fe6f477afff448dfe8ed2d3e1709f0ed
David A. Viramontes <dviramontes@gmail.com>
2018-08-03 22:40:54 -0600

    fix: add username

--- copy-ssh-keys.sh
+++ copy-ssh-keys.sh
@@ -3,9 +3,9 @@
 # run ssh-keygen if you haven't already
 # and then...
 
-ssh-copy-id 192.168.29.234
-ssh-copy-id 192.168.29.245
-ssh-copy-id 192.168.29.241
-ssh-copy-id 192.168.29.148
-ssh-copy-id 192.168.29.249
-ssh-copy-id 192.168.29.213
+ssh-copy-id pi@192.168.29.234
+ssh-copy-id pi@192.168.29.245
+ssh-copy-id pi@192.168.29.241
+ssh-copy-id pi@192.168.29.148
+ssh-copy-id pi@192.168.29.249
+ssh-copy-id pi@192.168.29.213
