75512147220c0a95f8206ad1188183eb436e4973
beavelar <bavelar@msudenver.edu>
2018-08-09 23:22:15 -0600

    adds/fixes loop

--- loop.sh
+++ loop.sh
@@ -4,9 +4,9 @@ type=$1
 while true; do
         if [[ $type == "master" ]]; then
                 echo "Streaming video"
-                avconv -re -i ../video1/INDONESIA.mp4 -vcodec copy -f a$
+                avconv -re -i ../video1/INDONESIA.mp4 -vcodec copy -f avi -an upd://239.0.1.23:1234
                 #call kill pwomplayer
-                avconv -re -i ../video1/C1_low.mp4 -vcodec copy -f avi $
+                avconv -re -i ../video1/C1_low.mp4 -vcodec copy -f avi -an upd://239.0.1.23:1234
         else
                 omxplayer ../video1/*.mp4
                 echo "Done with video"
@@ -16,7 +16,7 @@ while true; do
                 else
                         echo "Starting listening"
                         #pushd
-                        cd /home/pi && pwomxplayer -A udp://239.0.1.23:$
+                        cd /home/pi && pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B
                         #popd
                         cd ..
                 fi
