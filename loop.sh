1ba93cf99c646a7b9725d782be2a174228020cd9
beavelar <bavelar@msudenver.edu>
2018-08-09 23:09:34 -0600

    adds loop

---
+++ loop.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+type=$1
+while true; do
+        if [[ $type == "master" ]]; then
+                echo "Streaming video"
+                avconv -re -i ../video1/INDONESIA.mp4 -vcodec copy -f a$
+                #call kill pwomplayer
+                avconv -re -i ../video1/C1_low.mp4 -vcodec copy -f avi $
+        else
+                omxplayer ../video1/*.mp4
+                echo "Done with video"
+                if pgrep -x "pwomxplayer" > /dev/null
+                then
+                        echo "Already listening"
+                else
+                        echo "Starting listening"
+                        #pushd
+                        cd /home/pi && pwomxplayer -A udp://239.0.1.23:$
+                        #popd
+                        cd ..
+                fi
+        fi
+
+        echo "Playing second video"
+        #omxplayer ../*.mp4
+done
