e42f624a2911b24e9187486f93a7f2fa649f3572
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-08-12 13:08:15 -0600

    Fixes error in listen-piwall due to accidental relative directory

--- listen-piwall.sh
+++ listen-piwall.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 ssh pi@192.168.29.234 \
-	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> /dev/null & exit'
 
 ssh pi@192.168.29.245 \
-	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> /dev/null & exit'
 
 ssh pi@192.168.29.241 \
-	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> /dev/null & exit'
 
 ssh pi@192.168.29.148 \
-	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> /dev/null & exit'
 
 ssh pi@192.168.29.249 \
-	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> /dev/null & exit'
 
 ssh pi@192.168.29.213 \
-	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> /dev/null & exit'
