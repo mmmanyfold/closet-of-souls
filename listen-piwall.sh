3cd673da824b3b56c5a8e102fedab1854717b906
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-08-12 12:56:37 -0600

    Updates listen-piwall to prevent pwomxplayer from blocking

--- listen-piwall.sh
+++ listen-piwall.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 ssh pi@192.168.29.234 \
-	'pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' & \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
 
 ssh pi@192.168.29.245 \
-	'pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' & \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
 
 ssh pi@192.168.29.241 \
-	'pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' & \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
 
 ssh pi@192.168.29.148 \
-	'pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' & \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
 
 ssh pi@192.168.29.249 \
-	'pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' & \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
 
 ssh pi@192.168.29.213 \
-	'pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' & \
+	'nohup pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B &> dev/null & exit' \
