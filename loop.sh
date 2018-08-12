107111e28e613b18e48061034f16ebfc956ce58e
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-08-12 15:38:18 -0600

    Changes loop.sh to use wildcard and play videos from master based on directory

--- loop.sh
+++ loop.sh
@@ -4,25 +4,25 @@ while true; do
         ./listen-piwall.sh
         echo "Streaming video"
         # stream 1
-        avconv -re -i ../video1/INDONESIA.mp4 -vcodec copy -f avi -an udp://239.0.1.23:1234
+        avconv -re -i /home/pi/video1/*.mp4 -vcodec copy -f avi -an udp://239.0.1.23:1234
         # kill piwall listeners
         ./kill-piwall.sh pwomxplayer
         # play local 1 on tiles
         ./play-local.sh 1
         # play local 1
-        omxplayer /home/pi/video1/C1_low.mp4
+        omxplayer /home/pi/video1/local/*.mp4
         # end local 1 on tiles
         ./kill-piwall.sh omxplayer
         # start listening again
         ./listen-piwall.sh
         # stream 2
-        avconv -re -i ../video1/INDONESIA.mp4 -vcodec copy -f avi -an udp://239.0.1.23:1234
+        avconv -re -i /home/pi/video2/*.mp4 -vcodec copy -f avi -an udp://239.0.1.23:1234
         # kill piwall listeners
         ./kill-piwall.sh pwomxplayer
         # play local video 2
-        ./play-local.sh 1
+        ./play-local.sh 2
         # play local video 2
-        omxplayer /home/pi/video1/INDONESIA.mp4
+        omxplayer /home/pi/video2/local/*.mp4
         # end local 2 on tiles
         ./kill-piwall.sh omxplayer
 
