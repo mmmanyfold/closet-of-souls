df4c036908a7dbb41569d78cda8dd1e4eaafa035
ZadenRB <zaden.ruggieroboune@gmail.com>
2018-08-12 12:12:05 -0600

    Updates listen-piwall to close ssh tunnels instead of backgrounding each ssh

--- listen-piwall.sh
+++ listen-piwall.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 ssh pi@192.168.29.234 \
-	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B' & \
+	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' \
 
 ssh pi@192.168.29.245 \
-	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B' & \
+	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' \
 
 ssh pi@192.168.29.241 \
-	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B' & \
+	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' \
 
 ssh pi@192.168.29.148 \
-	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B' & \
+	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' \
 
 ssh pi@192.168.29.249 \
-	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B' & \
+	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit' \
 
 ssh pi@192.168.29.213 \
-	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B'
+	'pkill pwomxplayer; pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B & exit'
--- loop.sh
+++ loop.sh
@@ -2,11 +2,19 @@
 
 type=$1
 while true; do
+        ./listen-piwall.sh
         if [[ $type == "master" ]]; then
                 echo "Streaming video"
+                # stream 1
                 avconv -re -i ../video1/INDONESIA.mp4 -vcodec copy -f avi -an udp://239.0.1.23:1234
-                #call kill pwomplayer
-                avconv -re -i ../video1/C1_low.mp4 -vcodec copy -f avi -an udp://239.0.1.23:1234
+                # play local 1
+                omxplayer /home/pi/video1/C1_low.mp4
+                # kill and start listening again
+                ./listen-piwall.#!/bin/sh
+                # stream 2
+                avconv -re -i ../video1/INDONESIA.mp4 -vcodec copy -f avi -an udp://239.0.1.23:1234
+                # play local video 2
+                omxplayer /home/pi/video1/INDONESIA.mp4
         else
                 omxplayer ../video1/*.mp4
                 echo "Done with video"
