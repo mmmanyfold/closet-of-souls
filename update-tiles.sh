8fd6fa781861f6357b6f3b4be02a94641605f6b0
David A. Viramontes <dviramontes@gmail.com>
2018-08-12 22:42:28 -0600

    remove ips not

--- update-tiles.sh
+++ update-tiles.sh
@@ -6,9 +6,3 @@ scp .piwall pi@192.168.29.241:/home/pi
 scp .piwall pi@192.168.29.148:/home/pi
 scp .piwall pi@192.168.29.249:/home/pi
 scp .piwall pi@192.168.29.213:/home/pi
-scp .piwall pi@192.168.29.217:/home/pi
-scp .piwall pi@192.168.29.202:/home/pi
-scp .piwall pi@192.168.29.122:/home/pi
-scp .piwall pi@192.168.29.243:/home/pi
-scp .piwall pi@192.168.29.185:/home/pi
-scp .piwall pi@192.168.29.127:/home/pi
