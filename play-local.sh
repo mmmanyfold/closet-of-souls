1f603efc4e7750ede2913d8c8b0daad9e730743a
eemshi <oreneemshi@gmail.com>
2018-08-14 23:54:57 -0600

    force omxplayer audio thru headphone jack

--- play-local.sh
+++ play-local.sh
@@ -5,39 +5,39 @@ videoNum=$1
 # ./play-local.sh 1
 
 ssh pi@192.168.29.234 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.245 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.241 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.148 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.249 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.213 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.217 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.202 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.122 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.243 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.185 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 ssh pi@192.168.29.127 \
-  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 &> /dev/null & exit"
+  "nohup omxplayer -b /home/pi/video$videoNum/*.mp4 -o local &> /dev/null & exit"
 
 echo "Done starting videos"
